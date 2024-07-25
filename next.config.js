6b9368e55f7f88d5170e7485fd4a7586a4bab6bc
Jordan <github@jschr.io>
2024-07-25 10:40:57 -0400

    Image cache test

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+        source:
+          "/:path(.+\\.(?:ico|png|svg|jpg|jpeg|avif|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)",
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  // Reload the page to check for an updated image.
+  useEffect(() => {
+    setTimeout(() => {
+      router.reload();
+    }, 5000);
+  }, [router]);
+
+  return (
+    <main
+      className="flex min-h-screen flex-col items-center justify-between p-24 bg-contain bg-center bg-no-repeat"
+      style={{ backgroundImage: 'url("/image1.jpg")' }}
+    ></main>
+  );
 }
